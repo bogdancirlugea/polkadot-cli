c1399c2ee880d9c9934e7c4ca479d0e828d369ce
Bogdan Cirlugea <bogdancirlugea@nc-ass-vip.sdv.fr>
2020-10-18 14:09:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+const main = async () => {
+    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    // Retrieve the latest header
+    const lastHeader = await api.rpc.chain.getHeader();
+    const number = lastHeader.number.toString();
+    const hash = lastHeader.hash.toString();
+    const parentHash = lastHeader.parentHash.toString();
+    const stateRoot = lastHeader.stateRoot.toString();
+    const extrinsicsRoot = lastHeader.extrinsicsRoot.toString();
+    console.log(` Latest block:${number} \n Hash: ${hash} \n Parent hash: ${parentHash} \n Parent hash: ${stateRoot} \n Extrinsic root: ${extrinsicsRoot}`);
+}
+
+main().catch(err => {
+    console.log(err)
+}).finally(() => process.exit());
